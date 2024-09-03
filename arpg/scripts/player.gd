d915b8502d838e60f859fcf046e8af7d29bf2e43
DatWolfKiba <179305050+DatWolfKiba@users.noreply.github.com>
2024-09-03 11:46:48 +0300

    Add player knockback immunity.

--- arpg/scripts/player.gd
+++ arpg/scripts/player.gd
@@ -6,11 +6,16 @@ signal healthChanged
 
 @export var speed: int = 35
 @onready var animations: AnimationPlayer = $AnimationPlayer
+
 @export var maxHealth = 3
 @onready var currentHealth: int = maxHealth
 @export var knockbackPower: int = 500 
 @onready var effects = $Effects
 @onready var hurtTimer: Timer = $hurtTimer
+
+var isHurt: bool = false
+var enemyCollisions = []
+
 
 func _ready():
 	effects.play("RESET")
@@ -39,21 +44,35 @@ func _physics_process(delta):
 	move_and_slide()
 	handleCollisions()
 	updateAnimation()
+	if !isHurt:
+		for enemyArea in enemyCollisions:
+			hurtByEnemy(enemyArea)
+	
+func hurtByEnemy(area):
+	currentHealth -= 1
+	if currentHealth == 0:
+		currentHealth = maxHealth
+	healthChanged.emit(currentHealth)
+	isHurt = true
+	
+	knockback(area.get_parent().velocity)
+	effects.play("hurtBlink")
+	hurtTimer.start()
+	await hurtTimer.timeout
+	effects.play("RESET")
+	isHurt = false
+
 		
 func _on_hurt_box_area_entered(area):
 	if area.name == "hitBox":
-		currentHealth -= 1
-		if currentHealth == 0:
-			currentHealth = maxHealth
-		healthChanged.emit(currentHealth)
-		knockback(area.get_parent().velocity)
-		effects.play("hurtBlink")
-		hurtTimer.start()
-		await hurtTimer.timeout
-		effects.play("RESET")
-
+		enemyCollisions.append(area)
+		
 		
 func knockback(enemyVelocity: Vector2):
 	var knockbackDirection = (enemyVelocity - velocity).normalized() * knockbackPower
 	velocity = knockbackDirection
 	move_and_slide()	
+
+
+func _on_hurt_box_area_exited(area: Area2D) -> void:
+	enemyCollisions.erase(area) # Replace with function body.
