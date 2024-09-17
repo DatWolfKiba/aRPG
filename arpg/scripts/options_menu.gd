792efc02b1005efd10cacb0826e895823b1d0220
DatWolfKiba <179305050+DatWolfKiba@users.noreply.github.com>
2024-09-17 12:41:57 +0300

    Implement seperate sliders for audio and music.

--- arpg/scripts/options_menu.gd
+++ arpg/scripts/options_menu.gd
@@ -5,11 +5,5 @@ extends Control
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn") 
 
-
-func _on_h_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(0, value/5)
-
-
-
 func _on_check_box_toggled(toggled_on: bool) -> void:
 	AudioServer.set_bus_mute(0, toggled_on)
