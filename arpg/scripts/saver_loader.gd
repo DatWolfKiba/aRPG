a00360dee1b1654768837180cee74dfb9c6c049f
DatWolfKiba <179305050+DatWolfKiba@users.noreply.github.com>
2024-09-22 00:15:57 +0300

    Implement basic save with dictionary format choice.

--- arpg/scripts/saver_loader.gd
+++ arpg/scripts/saver_loader.gd
@@ -2,28 +2,22 @@ class_name SaverLoader
 extends Node
 @onready var player: Player = $"../../Player"
 
-
-# Called when the node enters the scene tree for the first time.
 func save_game() -> void:
-	var file = FileAccess.open("user://savegame.json", FileAccess.WRITE)
+	var file = FileAccess.open("user://savegame.data", FileAccess.WRITE)
 	
 	var saved_data = {}
 	
-	saved_data["player_position:x"] = player.global_position.x
-	saved_data["player_position:y"] = player.global_position.y
+	saved_data["player_position"] = player.global_position
 	
 	var json = JSON.stringify(saved_data)
 	
-	file.store_string(json)
+	file.store_var(saved_data)
 	file.close()
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func load_game() -> void:
-	var file = FileAccess.open("user://savegame.json", FileAccess.READ)
-	var json = file.get_as_text()
-	
-	var saved_data = JSON.parse_string(json)
-	
-	player.global_position.x = saved_data["player_position:x"] 
-	player.global_position.y = saved_data["player_position:y"] 
 
+func load_game() -> void:
+	var file = FileAccess.open("user://savegame.data", FileAccess.READ)
+
+	var saved_data = file.get_var()
+	
+	player.global_position = saved_data["player_position"] 
 	file.close()
