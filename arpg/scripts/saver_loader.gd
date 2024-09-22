3b96be554e359fcf64cb7d53cde10768a4cc1f31
DatWolfKiba <179305050+DatWolfKiba@users.noreply.github.com>
2024-09-22 10:45:02 +0300

    Implement save with resources for player healt.

--- arpg/scripts/saver_loader.gd
+++ arpg/scripts/saver_loader.gd
@@ -9,7 +9,9 @@ func save_game() -> void:
 		
 	ResourceSaver.save(saved_game, "user://savegame.tres")
 func load_game() -> void:
-	var saved_game:SavedGame = load("user://savegame.tres") as SavedGame
-	
-	player.global_position = saved_game.player_position
-	player.currentHealth = saved_game.player_health
+	var saved_game: SavedGame = load("user://savegame.tres") as SavedGame
+
+	if saved_game:
+		player.global_position = saved_game.player_position
+		player.currentHealth = saved_game.player_health
+		player.emit_signal("healthChanged", player.currentHealth)  # Update the health in the GUI
